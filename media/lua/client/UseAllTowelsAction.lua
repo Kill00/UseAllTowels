2166bdf3b8509b164d65982de85a38566ae5c0ae
Kill00 <kill00@p.teamvery.dev>
2024-01-13 19:19:47 +0900

    0.0.3 - 실시간 사용 표시

--- media/lua/client/UseAllTowelsAction.lua
+++ media/lua/client/UseAllTowelsAction.lua
@@ -7,13 +7,44 @@ function useAllTowelsAction:isValid()
 end
 
 function useAllTowelsAction:start()
+    self.count = 0
     self.nowWetness = self.character:getBodyDamage():getWetness()
     self.targetWetness = self.nowWetness * 0.6
+    self.remainWetness = self.nowWetness - self.targetWetness
+    self.finished = false
+
+    if (self.character:getBodyDamage():getWetness() == 0 or self.item:getUsedDelta() == 0) then
+        self:forceComplete()
+    end
 end
 
-function useAllTowelsAction:update()
-    if (self.character:getBodyDamage():getWetness() == 0) then
+function useAllTowelsAction:update() -- 배속 하면 이상해짐
+    local jobDelta = self:getJobDelta() * 100
+
+    if (self.character:getBodyDamage():getWetness() == 0 or self.item:getUsedDelta() == 0) then
+        self.finished = false
         self:forceComplete()
+    end
+
+    local needCount = math.floor(jobDelta * 0.1)
+    local jotDeltaInt = math.floor(jobDelta)
+    local canDo = needCount ~= self.count and jotDeltaInt % 10 < 10
+
+    if (self.count ~= needCount) then
+        self.count = self.count + 1
+    end
+
+    if (canDo) then
+        local nowItemWetness = self.item:getUsedDelta()
+        if (nowItemWetness >= 0.1) then
+            self.remainWetness = self.remainWetness - (self.nowWetness - self.targetWetness) * 0.1
+            self.character:getBodyDamage():decreaseBodyWetness((self.nowWetness - self.targetWetness) * 0.1)
+            self.item:setUsedDelta(nowItemWetness - 0.1)
+            self.finished = true
+        else
+            self.finished = true
+            self:forceComplete()
+        end
     end
 end
 
@@ -21,16 +52,18 @@ function useAllTowelsAction:stop()
     ISBaseTimedAction.stop(self)
 end
 
-function useAllTowelsAction:perform()
-    local wetness = self.character:getBodyDamage():getWetness()
-    if (self.isBathTowel or self.isDishCloth) then
-        if (self.nowWetness > 0) then
-            if (wetness > self.targetWetness) then
-                self.character:getBodyDamage():decreaseBodyWetness(self.nowWetness - self.targetWetness)
+function useAllTowelsAction:perform() -- Update 보완
+    if (self.finished) then
+        if (self.isBathTowel or self.isDishCloth) then
+            if (self.nowWetness > 0) then
+                self.character:getBodyDamage():decreaseBodyWetness(self.remainWetness)
                 self.item:setUsedDelta(0)
-                self.item:Use()
             end
         end
+    end
+
+    if (self.item:getUsedDelta() == 0) then
+        self.item:Use()
     end
 
     ISBaseTimedAction.perform(self)
@@ -50,6 +83,10 @@ function useAllTowelsAction:new(character, item)
     o.isDishCloth = false
     o.nowWetness = 0
     o.targetWetness = 0
+    o.remainWetness = 0
+
+    o.finished = false
+    o.count = 0
 
     if (item:getType() == "BathTowel") then
         o.isBathTowel = true
